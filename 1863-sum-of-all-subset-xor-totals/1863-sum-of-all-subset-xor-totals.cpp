edffd46b6c40f83c1ad071199796fa668504e94b
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-06-29 23:59:11 +0530

    Time: 7 ms (56.06%), Space: 7.1 MB (56.13%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,22 +1,44 @@
 class Solution {
 public:
     int subsetXORSum(vector<int>& nums) {
-       
         int ans=0;
-        for(int i=0; i<32; i++)
-        {
-            int mask=1<<i;
-            int count=0;
-            for(int j=0; j<nums.size(); j++)
-            {
-                if(nums[j]&mask) count++;
+        int p=0,ans1=0;
+        int ans2=0;
+        
+        for(int i=0;i<32;i++){
+            int countz=0;
+            int countO=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]&(1<<i))
+                    countO++;
+                else
+                    countz++;
             }
-            if(count)
-            {
-                ans+=mask*(1<<(count-1))*(1<<(nums.size()-count));
-            }
+            if(countO)
+            { p=(1<<(countO-1))*(1<<countz);
+            ans+=p*(1<<i);}
         }
-        return ans;
-   
+            return ans+ans1+ans2;
     }
 };
+
+
+
+   // int subsetXORSum(vector<int>& nums) 
+   //  {
+   //      int ans=0;
+   //      for(int i=0; i<32; i++)
+   //      {
+   //          int mask=1<<i;
+   //          int count=0;
+   //          for(int j=0; j<nums.size(); j++)
+   //          {
+   //              if(nums[j]&mask) count++;
+   //          }
+   //          if(count)
+   //          {
+   //              ans+=mask*(1<<(count-1))*(1<<(nums.size()-count));
+   //          }
+   //      }
+   //      return ans;
+   //  }
