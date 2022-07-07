af802467f2f5f6014ef8ca5f79d5450a9f772518
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-07-07 18:28:01 +0530

    Time: 9 ms (34.50%), Space: 7.5 MB (73.21%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
-public:
-    void subsum(set<vector<int>> &res,int i,int n,vector<int> &a,vector<int> &dp){
-         if(i==n){
-             res.insert(dp);
-             return;
+   private:
+      void findSubsets(int ind, vector < int > & nums, vector < int > & ds, vector < vector < int >> & ans) {
+         ans.push_back(ds);
+         for (int i = ind; i < nums.size(); i++) {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            ds.push_back(nums[i]);
+            findSubsets(i + 1, nums, ds, ans);
+            ds.pop_back();
          }
-         dp.push_back(a[i]);
-         subsum(res,i+1,n,a,dp);
-         dp.pop_back();
-         subsum(res,i+1,n,a,dp);
-     }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-        set<vector<int>> res;
-        vector<int> dp;
-       int i=0,sum=0;
-      subsum(res,i,nums.size(),nums,dp);
-        vector<vector<int>> res1(res.begin(),res.end());
-      return res1; 
-    }
+      }
+   public:
+      vector < vector < int >> subsetsWithDup(vector < int > & nums) {
+         vector < vector < int >> ans;
+         vector < int > ds;
+         sort(nums.begin(), nums.end());
+         findSubsets(0, nums, ds, ans);
+         return ans;
+      }
 };
