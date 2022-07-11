e0dcf957dfd6f25bfe0165fb23e0960009c1ec2f
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-07-11 14:55:24 +0530

    Time: 6 ms (54.29%), Space: 7.5 MB (89.64%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,7 +1,11 @@
 class Solution {
    private:
       void findSubsets(int ind, vector < int > & nums, vector < int > & ds, vector < vector < int >> & ans) {
-         ans.push_back(ds);
+          ans.push_back(ds);
+          if(ind==nums.size())
+          {
+         return;
+          }
          for (int i = ind; i < nums.size(); i++) {
             if (i != ind && nums[i] == nums[i - 1]) continue;
             ds.push_back(nums[i]);
