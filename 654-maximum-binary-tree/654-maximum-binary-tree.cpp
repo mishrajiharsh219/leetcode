2d213a2db9b096a7e80b3a13ba617ba98a39bc20
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-08-24 00:43:47 +0530

    Time: 140 ms (52.31%), Space: 42.2 MB (45.85%) - LeetHub

--- 654-maximum-binary-tree/654-maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/654-maximum-binary-tree.cpp
@@ -1,30 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    
-    TreeNode* constructTree(int s,int e,vector<int> &nums){
-        if(s>e)
-            return NULL;
+    TreeNode* helper(int s,int e,vector<int> &nums){
         int idx=maxi(s,e,nums);
+       if(s>e)
+           return NULL;
         TreeNode *root=new TreeNode(nums[idx]);
-        root->left=constructTree(s,idx-1,nums);
-        root->right=constructTree(idx+1,e,nums);
+        root->left=helper(s,idx-1,nums);
+        root->right=helper(idx+1,e,nums);
         return root;
     }
     
-    
     int maxi(int i,int j,vector<int> &nums){
-        int max=INT_MIN,index=-1;
-       for(int s=i;s<=j;s++){
-           if(nums[s]>max){
-               max=nums[s];
-       index=s;
-       }}
-        return index;
+        int indx=-1,m=INT_MIN;
+        for(int a=i;a<=j;a++){
+            if(nums[a]>m){
+                m=nums[a];
+                indx=a;
+            }
+        }
+            return indx;
     }
     
     TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-      int n=nums.size();
-        TreeNode *root=constructTree(0,n-1,nums);
+        int n=nums.size();
+        TreeNode* root=helper(0,n-1,nums);
         return root;
     }
 };
