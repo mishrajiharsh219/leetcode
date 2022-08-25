88acb1d8aea47e749d5c484f9b3c72d406ff23ee
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-08-25 12:10:49 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.76%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void helper(vector<string> &res,string &digits,map<int,string> &mp,string &ds,int i){
+        if(i==digits.size()){
+            res.push_back(ds);
+            return;
+        }
+      for(auto c:mp[digits[i]-'0']){
+          ds.push_back(c);
+          helper(res,digits,mp,ds,i+1);
+          ds.pop_back();
+        }
+        return;
+    }
+    vector<string> letterCombinations(string digits) {
+         if(digits=="")
+             return {};
+        map<int,string> mp;
+        mp[2]="abc";
+        mp[3]="def";
+        mp[4]="ghi";
+        mp[5]="jkl";
+        mp[6]="mno";
+        mp[7]="pqrs";
+        mp[8]="tuv";
+        mp[9]="wxyz";
+        vector<string> res;
+        string ds="";
+        helper(res,digits,mp,ds,0);
+        return res;
+    }
+};
