bda28ec68059a786c3581500574c375f68c2d6b0
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-08-27 07:20:08 +0530

    Time: 1376 ms (76.73%), Space: 293.5 MB (42.82%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-	// TIME COMPLEXITY:- O(M^2 NLOG(N))
-	// SPACE COMPLEXITY:- O(N)
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        int ans = INT_MIN;
-        // prefix sum of every row
+        int n=matrix.size();
+        int m=matrix[0].size();
         for(int i=0;i<n;i++){
             for(int j=1;j<m;j++)
                 matrix[i][j]+=matrix[i][j-1];
         }
-        // try every possible width of subarray
+        int ans=INT_MIN;
         for(int start=0;start<m;start++){
             for(int end=start;end<m;end++){
-                set<int> s = {0};
-                int pref_sum = 0;
-                // for current width of rectangle
+                set<int> s={0};
+                   int pre_sum=0;
                 for(int i=0;i<n;i++){
-                    int sum = matrix[i][end];
-                    if(start>0)
-                        sum-=matrix[i][start-1];
-                    pref_sum+=sum;
-                    auto itr = s.lower_bound(pref_sum-k);
+                      int sum=matrix[i][end];
+                if(start>0)
+                    sum-=matrix[i][start-1];
+                    pre_sum+=sum;
+                    auto itr=s.lower_bound(pre_sum-k);
                     if(itr!=s.end())
-                        ans = max(ans,pref_sum-(*itr));
-                    s.insert(pref_sum);
+                        ans=max(pre_sum-(*itr),ans);
+                    s.insert(pre_sum);
                 }
             }
         }
