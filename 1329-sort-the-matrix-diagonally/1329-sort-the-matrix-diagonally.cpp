2c295edf4f3a241259c0dbecab2884dcca485c28
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-08-29 13:36:00 +0530

    Time: 25 ms (24.29%), Space: 9.2 MB (70.32%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        vector<int> k;
-        int m = mat.size() , n = mat[0].size();
-        for (int r=0; r<m; r++)        // r-> row
-        {
-            k.clear();
-            for (int j=0,i=r; j<n && i<m ; j++,i++) k.push_back(mat[i][j]);    // add to vector
-            sort(k.begin(),k.end());
-            for (int j=0,i=r; j<n && i<m ; j++,i++) mat[i][j]=k[j];            // replace
-        }
-        for (int c=1; c<n; c++)       // c->column
-        {
-            k.clear();
-            for (int i=0, j=c; j<n && i<m; i++, j++) k.push_back(mat[i][j]);   // add to vector
-            sort(k.begin(),k.end());
-            for (int i=0, j=c; j<n && i<m; i++, j++) mat[i][j] = k[i];         //replace
+       int n=mat.size();
+        int m=mat[0].size();
+        int rowindex=0;
+        int colindex;//diagonal at col=0 and row=0 is same
+        int diagonals=n+m-1;
+        for(int i=1;i<=diagonals;i++){
+            vector<int> diagonal;
+            
+            if(i<=m){
+                colindex=m-i;
+            }
+            else{
+                rowindex++;
+                colindex=0;
+            }
+            for(int i=rowindex,j=colindex;i<n and j<m;i++,j++){
+                diagonal.push_back(mat[i][j]);
+            }
+            sort(diagonal.begin(),diagonal.end());
+            for(int i=rowindex,j=colindex,k=0;i<n and j<m;i++,j++,k++){
+                mat[i][j]=diagonal[k];
+            }
         }
         return mat;
     }
