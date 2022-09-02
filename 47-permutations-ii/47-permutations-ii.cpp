06f34b00e885fd2b3ea8750d3f8cbf9293aadb0a
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-09-02 22:58:09 +0530

    Time: 161 ms (14.25%), Space: 9.3 MB (57.41%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,17 +1,20 @@
-//we can also use unordered map
-// void backtrack2(int ind, int prev, int k, int n, vector<int>& temp,
-// 			   vector<vector<int>>& res) {
-// 	if (ind >= k) {
-// 		res.push_back(temp);
-// 		return;
-// 	}
-// 	for (int p = prev + 1; p <= n; p++) {
-// 		int saved = temp[ind]; // Given the way how we fill temp array - this is not necessary.
-// 		temp[ind] = p;
-// 		backtrack2(ind + 1, p, k, n, temp, res);
-// 		temp[ind] = saved;     // Given the way how we fill temp array - this is not necessary.
-// 	}
-// }
+// // Iterate over the original list, but check if the previous element is the same as current. 
+// // We need to make this check because using the same element will give us the same result as last iteration.
+// void backtrack2(vector<int>& nums, vector<int>& temp,
+//                   vector<vector<int>>& res, unordered_map<int, int>& freq) {
+//         if (temp.size() == nums.size()) {
+//             res.push_back(temp);
+//             return;
+//         }
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (freq[nums[i]] == 0 || (i != 0 && nums[i] == nums[i - 1])) continue;
+//             temp.push_back(nums[i]);
+//             freq[nums[i]]--;
+//             backtrack(nums, temp, res, freq);
+//             freq[nums[i]]++;
+//             temp.pop_back();
+//         }
+//     }
 class Solution {
 public:
     void backtrack(set<vector<int>> &res,vector<int> &ds,vector<int> &nums,vector<int> &freq)
