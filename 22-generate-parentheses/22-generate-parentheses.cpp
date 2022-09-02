d256a453f30b6611ddfee21da6355484e61e6c57
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-09-03 02:59:14 +0530

    Time: 7 ms (55.29%), Space: 13.6 MB (60.03%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,3 +1,17 @@
+ /* The main idea here is:
+        1.  In order to generate valid combinations of parentheses, we need to make sure that
+            there is a closing parentheses for every open parentheses. This can be achieved by
+            taking care of the following points:
+            a.  At any point, no. of open parentheses > no. of closing parentheses
+            b.  If no. of open parentheses == no. of closing parentheses, we have obtained a valid pattern
+                (this will be our terminating condition for recursion)
+            
+        2.  We can repeat the process in point 1, until we have obatained all the possible combinations.
+        
+        */
+        
+        /*if no. of open and closing parentheses is equal to n, add the  
+        combination to the list and return (and thus end the recursion)*/
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
