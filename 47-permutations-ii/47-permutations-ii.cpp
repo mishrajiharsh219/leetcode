27913139ae6e7575ef8a546d3299380ed395d7b9
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-09-02 22:57:46 +0530

    Time: 136 ms (15.39%), Space: 9.3 MB (60.86%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,3 +1,17 @@
+//we can also use unordered map
+// void backtrack2(int ind, int prev, int k, int n, vector<int>& temp,
+// 			   vector<vector<int>>& res) {
+// 	if (ind >= k) {
+// 		res.push_back(temp);
+// 		return;
+// 	}
+// 	for (int p = prev + 1; p <= n; p++) {
+// 		int saved = temp[ind]; // Given the way how we fill temp array - this is not necessary.
+// 		temp[ind] = p;
+// 		backtrack2(ind + 1, p, k, n, temp, res);
+// 		temp[ind] = saved;     // Given the way how we fill temp array - this is not necessary.
+// 	}
+// }
 class Solution {
 public:
     void backtrack(set<vector<int>> &res,vector<int> &ds,vector<int> &nums,vector<int> &freq)
