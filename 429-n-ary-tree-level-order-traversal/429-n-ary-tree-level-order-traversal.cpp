0c33b4698cf9f7c5d83cc4f1243d924b5eeb430d
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-09-05 15:55:52 +0530

    Time: 46 ms (24.79%), Space: 11.8 MB (34.37%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -21,21 +21,21 @@ public:
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
-           if (root == NULL) 
-			return {};
-        vector<vector<int>> res;
+          if(root==NULL)
+              return {};
         queue<Node*> q;
         q.push(root);
+        vector<vector<int>> res;
         while(!q.empty()){
-            vector<int> currlevel;
             int size=q.size();
+            vector<int> currlevel;
             for(int i=0;i<size;i++){
-            Node* temp=q.front();
-            q.pop();
-            currlevel.push_back(temp->val);
-            for(auto c: temp-> children)
-                q.push(c);
-        }
+                Node* temp=q.front();
+                q.pop();
+                currlevel.push_back(temp->val);
+                for(auto c:temp->children)
+                    q.push(c);
+            }
             res.push_back(currlevel);
         }
         return res;
