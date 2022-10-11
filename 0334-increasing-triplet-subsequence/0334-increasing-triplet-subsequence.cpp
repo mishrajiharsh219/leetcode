d2a919fec7d21017de92232d7c14a9e5ccdb9e5c
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-10-11 21:37:59 +0530

    Time: 51 ms (99.90%), Space: 61.6 MB (24.04%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& arr) {
-  int n=arr.size();
-        int i;
-        int small=INT_MAX,large=INT_MAX;
-        //Hum yha pe do variable liye hai small and large jismei hum extreme large number daal diye
-        for(int i=0;i<n;i++)
-        {
-            //ismei sb se small number save ho jayega 
-            if(arr[i]<=small)
-                small=arr[i];
-            //ismei loop run k baad small se bda number save hoga
-            else if(arr[i]<=large)
-                large=arr[i];
-            //And agar large se bda v koi number hoga toa ye true return kr dega
+    bool increasingTriplet(vector<int>& nums) {
+      int small=INT_MAX,mid=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=small)
+            {
+                small=nums[i];
+            }
+            else if(nums[i]<=mid and nums[i]>small){
+                mid=nums[i];
+            }
             else
                 return true;
         }
-        // or agar upper condition sb satisfy nhi karega toa false return kr dega
-            return false;
-       //if you like then please vote 
+        return false;
     }
 };
