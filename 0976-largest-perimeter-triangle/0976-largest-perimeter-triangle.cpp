b873d9ae3be5b14cbb61442f20fc68a7a5d9a098
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-10-12 17:15:00 +0530

    Time: 41 ms (91.76%), Space: 21.8 MB (96.06%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -1,4 +1,6 @@
 //sum of any two sides of atriangle is greater than the third side
+//meansa+b>c and b+c>a and c+a>b
+//if we proof smallest two sides of triangle sum> biggest sides then all conditions are automatically fullfilled
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
