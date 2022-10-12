69bb4e4d37a43731a210678baf3f1f29e478cc9f
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-10-12 19:02:55 +0530

    Time: 6 ms (39.25%), Space: 7.9 MB (12.16%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool valid(vector<int> nums,int k,int curr_min){
+    int n=nums.size();
+        int grpsrequired=1;
+        int currsum=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>curr_min){
+                return false;
+            }
+            if(currsum+nums[i]>curr_min){
+                grpsrequired++;
+                currsum=nums[i];
+                if(grpsrequired>k)
+                    return false;
+            }
+            else
+                currsum+=nums[i];
+        }
+        return true;
+    }
+    
+    int splitArray(vector<int>& nums, int k) {
+        long long sum=0;
+        int low=0;
+        for(int i=0;i<nums.size();i++)
+            sum+=nums[i];
+        int high=sum;
+        int res=INT_MAX;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(valid(nums,k,mid)){
+                res=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+};
