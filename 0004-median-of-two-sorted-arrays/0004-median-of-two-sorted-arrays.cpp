db22e654cd3d33009dcacb464464712312cb87d2
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-10-14 19:55:42 +0530

    Time: 23 ms (99.87%), Space: 89.4 MB (65.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,28 +1,48 @@
+// "if we cut the sorted array to two halves of EQUAL LENGTHS, then
+// median is the AVERAGE OF Max(lower_half) and Min(upper_half), i.e. the
+// two numbers immediately next to the cut
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        int finalsize=n+m;
-        int i=0,j=0,k=0;
-        vector<int> merged(finalsize,-1);
-        while(i<n and j<m){
-            if(nums1[i]<nums2[j])
-            merged[k++]=nums1[i++];
-            else
-            merged[k++]=nums2[j++];    
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        // if size of nums2 array is greater than nums1
+        if(m > n) {
+			// because we will use binary search search therefore starting with lower size of array
+            return findMedianSortedArrays(nums2, nums1);
         }
-        if(i<n){
-            while(i<n)
-            merged[k++]=nums1[i++];
+		
+        int s = 0, e = min(m,n);
+        while(s<=e) {
+			// find mid for first array
+            int cut1 = s + (e-s)/2;
+			// find mid for second array
+            int cut2 = (n+m+1)/2 - cut1;
+            
+			/* if mid == 0 then assign MIN && MAX value accordingly
+			to avoid errors while comparing for both arrays */
+			
+			// l1 and l2 are left element of mid or cut1
+            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+			//  r1 and r2 are right element or mid
+            int r1 = cut1 == m ? INT_MAX : nums1[cut1];
+            int r2 = cut2 == n ? INT_MAX : nums2[cut2];
+            
+			// below condition means that both arrays are logically merged and sorted
+            if(l1 <= r2 && l2 <= r1) {
+				// if total no of elements of both arrays are even
+                if((n+m)%2==0) {
+                    return ((max(l1,l2) + min(r1,r2))/2.0);
+                }
+				// if elements are odd
+                else return max(l1,l2);
+            }
+            else if(l2 > r1) {
+                s = cut1+1;
+            }
+            else e = cut1-1;
         }
-        if(j<m){
-            while(j<m)
-            merged[k++]=nums2[j++];
-        }
-        if(finalsize%2==1){
-            return merged[((finalsize+1)/2)-1];
-        }
-           return ((float)merged[(finalsize/2)-1]+(float)merged[(finalsize/2)])/2;
+        return 0.0;
     }
 };
