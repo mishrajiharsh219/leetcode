4169eeda5d9316ca2f8f9ae873032a12b4f94c02
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-11-09 15:00:48 +0530

    Time: 554 ms (18.79%), Space: 84.3 MB (22.76%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,24 @@
+class StockSpanner {
+    stack<pair<int,int> > s;
+public:
+    StockSpanner() {
+        
+    }
+    //if current element is smaller than the current element then span will be 1
+    //else span of current element will depend on the span of previous elements which are smaleer than current element
+    int next(int price) {
+        int span=1;
+        while(!s.empty() && s.top().first<=price){
+            span+=s.top().second;
+            s.pop();
+        }
+        s.push({price,span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
