ea2426f864640d6272f91117184785e4024310c7
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-11-13 23:41:52 +0530

    Time: 7 ms (81.74%), Space: 7.1 MB (91.09%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        reverse(s.begin(),s.end());
-        int i=0,j=0,n=s.size(),lastindex=0;
+       reverse(s.begin(),s.end());
+        int i=0,j=0,lastindex=0,n=s.length();
         while(j<n){
             while(j<n and s[j]==' ')
                 j++;
@@ -12,7 +12,7 @@ public:
                 lastindex=i;
             }
             reverse(s.begin()+startindex,s.begin()+i);
-            s[i++]=' ';
+            s[i++]=' '; 
         }
         s.resize(lastindex);
         return s;
