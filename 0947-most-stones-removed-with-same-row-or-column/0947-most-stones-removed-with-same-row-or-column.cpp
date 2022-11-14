0e57789017a4455d3da8f4ccbe2b458ce8706c88
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-11-14 21:06:55 +0530

    Time: 813 ms (6.23%), Space: 14.3 MB (80.50%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -4,21 +4,18 @@ public:
     void dfs(vector<vector<int>>& stones,int i,vector<int> &vis){
         if(vis[i]==0)
             vis[i]=1;
-            
-            for(int j=0;j<stones.size();j++){
-                if(vis[j]==0 and (stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1]))
-                {
-                    ans++;
-                    dfs(stones,j,vis);
-                }
+        for(int j=0;j<stones.size();j++){
+            if(vis[j]==0 and(stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1])){
+                ans++;
+                dfs(stones,j,vis);
             }
+        }
     }
     int removeStones(vector<vector<int>>& stones) {
         vector<int> vis(stones.size(),0);
-        
+
         for(int i=0;i<stones.size();i++){
-            if(vis[i]==0)
-                dfs(stones,i,vis);
+            dfs(stones,i,vis);
         }
         return ans;
     }
