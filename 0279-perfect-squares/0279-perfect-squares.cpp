e63a7b579d2d57805bb035180c34320b90e0d23d
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2022-11-22 20:43:35 +0530

    Time: 105 ms (88.67%), Space: 6 MB (91.59%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -5,10 +5,10 @@ public:
         for(int i=0;i<=n;i++)
             dp[i]=INT_MAX;
         dp[0]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j*j<=i;j++)
+        for(int i=1;i<=n;i++)
+            for(int j=1;j*j<=i;j++){
                 dp[i]=min(dp[i],dp[i-j*j]+1);
-        }
+            }
         return dp[n];
     }
 };
