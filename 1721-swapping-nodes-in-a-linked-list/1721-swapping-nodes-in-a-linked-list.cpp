7e6c23cf6d815c726dddffc35a58f65f08ff0d72
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-02-11 17:02:56 +0530

    Time: 691 ms (69.78%), Space: 180.1 MB (62.02%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,65 +10,19 @@
  */
 class Solution {
 public:
-    int size(ListNode* head){
-        int s=0;
-        while(head){
-            head=head->next;
-            s++;
-        }
-        return s;
-    }
     ListNode* swapNodes(ListNode* head, int k) {
-        if(head==NULL or head->next==NULL)
-            return head;
-        int n=size(head);
-   
-        ListNode* newhead=new ListNode(-1);
-         newhead->next=head;
-        ListNode* org=newhead;
-        ListNode* newend=new ListNode(-1);
-        while(org->next){
-            org=org->next;
-        }
-        org->next=newend;
+        ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
+        while (--k)
+            ptr1 = ptr1->next;
         
-        ListNode* prev1=newhead,*prev2=newhead,*temp1,*temp2,*next1,*next2;
-        int firstshift=k-1;
-        int secondshift=n-k;
-        while(firstshift--){
-            prev1=prev1->next;
-        }
-        while(secondshift--){
-            prev2=prev2->next;
-        }
-        temp1=prev1->next;
-        temp2=prev2->next;
-        next1=prev1->next->next;
-        next2=prev2->next->next;
+        kth = ptr1;
+        ptr1 = ptr1->next;
         
-        //edge case if both elements are adjacent
-        
-        if(temp1->next==temp2){
-         prev1->next=temp2;   
-         temp2->next=temp1;
-        temp1->next=next2;
+        while (ptr1) {
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
         }
-        else if(temp2->next==temp1){
-             prev2->next=temp1;   
-         temp1->next=temp2;
-        temp2->next=next1;
-        }
-        else{
-        prev1->next=temp2;
-        temp2->next=next1;
-        prev2->next=temp1;
-        temp1->next=next2;
-        }
-        ListNode* ptr=newhead;
-        while(n--){
-            ptr=ptr->next;
-        }
-        ptr->next=NULL;
-        return newhead->next;
+        swap(ptr2->val, kth->val);
+        return head;
     }
 };
