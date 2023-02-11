61cab9470815f6737b98ab1d8f801a0c208e1502
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-02-11 23:24:21 +0530

    Time: 325 ms (98.39%), Space: 93.1 MB (90.08%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.size();
-        vector<int> sum(n+1, 0);
-        for(int i = 0; i<shifts.size(); i++){
-            int x = shifts[i][0], y = shifts[i][1]+1;
-            int dir = shifts[i][2];
-            if(dir == 1){
-                sum[x] +=1;
-                sum[y] -= 1;
+        int n=s.size(),sum=0;
+        vector <int> v(n+1,0);
+        
+        for(int i=0;i<shifts.size();i++){
+            int l=shifts[i][0];
+            int r=shifts[i][1];
+            if(shifts[i][2]==0){
+                v[l]--;
+                v[r+1]++;
             }
             else{
-                sum[x] -=1;
-                sum[y] +=1;
+                v[l]++;
+                v[r+1]--;
             }
         }
-        for(int i = 1; i<=n; i++){
-            sum[i] += sum[i-1];
-        }
-        for(int i = 0; i<n; i++){
-            int a = sum[i];
-            if(a<0){
-                int b = abs(a)%26;
-                a = s[i]-'a'+26-b;
-                s[i] = 'a'+a%26;
-            }   
-            else{
-                a += s[i]-'a';
-                s[i] = 'a' + a%26;
-            }
+    
+        for(int i=0;i<n;i++){
+            sum+=v[i];
+            int k=sum%26;
+            int p=(s[i]-'a'+k+26)%26;
+            s[i]=char('a'+p);
         }
         return s;
-
     }
 };
