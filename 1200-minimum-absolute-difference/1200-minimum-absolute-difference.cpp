99d2cdf73f659ca698fe7cbc5068b525ab11c1c7
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-02-15 23:00:48 +0530

    Time: 62 ms (99.03%), Space: 32.2 MB (79.42%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -6,9 +6,12 @@ public:
         int n=arr.size();
         int mini=INT_MAX;
         for(int i=0;i<n-1;i++){
-            mini=min(mini,abs(arr[i]-arr[i+1]));
-        }
-        for(int i=0;i<n-1;i++){
+           if(abs(arr[i]-arr[i+1])<mini)
+           {
+               mini=abs(arr[i]-arr[i+1]);
+               ans.clear();
+           }
+            
             if(abs(arr[i]-arr[i+1])==mini)
                ans.push_back({arr[i],arr[i+1]}); 
         }
