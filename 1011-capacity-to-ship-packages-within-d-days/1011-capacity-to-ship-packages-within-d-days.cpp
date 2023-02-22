4d0f4ab7aae0744bccfeeda6d9516aad47aac460
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-02-22 09:41:20 +0530

    Time: 64 ms (62.89%), Space: 45.6 MB (5.75%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,37 @@
-//binary search over solution domain
-//finding first bad version over solution type question
 class Solution {
 public:
- bool helper(int mid,vector<int>& weights,int days){
-      int count=1;
-     int sum=0;
-     for(int i=0;i<weights.size();i++){
-         sum+=weights[i];
-         if(sum>mid){
-             count++;
-            sum=weights[i];
-         }
-     }
-     if(count<=days)
-         return true;
-     return false;
+    bool valid(vector<int> w,int mid,int days)
+    {
+        int count=1;
+        int sum=0;
+        for(int i=0;i<w.size();i++)
+        {
+            sum+=w[i];
+            if(sum>mid)
+            {
+                count++;
+                sum=w[i];
+            }
+        }
+        if(count>days)
+            return false;
+        return true;
     }
-    
     int shipWithinDays(vector<int>& weights, int days) {
         int n=weights.size();
-        int ans=0;
-        int maxcap=0;//can be done in single day,day=1;
-        int mincap=*max_element(weights.begin(),weights.end());//if single element each day,days=n
-        for(int i=0;i<weights.size();i++){
+        int mincap=*max_element(weights.begin(),weights.end());
+        int maxcap=0;
+        for(int i=0;i<n;i++)
             maxcap+=weights[i];
-        }
-        int high=maxcap;
         int low=mincap;
-        while(low<high){
-            int mid=low + (high-low)/2;
-            if(helper(mid,weights,days)){
+        int high=maxcap;
+        while(low<high)
+        {
+            int mid=low+(high-low)/2;
+            if(valid(weights,mid,days))
                 high=mid;
-            }
             else
-            low=mid+1;
+                low=mid+1;
         }
         return low;
     }
