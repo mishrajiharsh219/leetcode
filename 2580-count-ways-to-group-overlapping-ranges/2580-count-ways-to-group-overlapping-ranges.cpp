a2ac5ce14dc996e1d3118143905da66f4ff0443a
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-08 23:03:09 +0530

    Time: 345 ms (64.89%), Space: 72.7 MB (69.48%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.cpp
@@ -3,7 +3,6 @@ public:
     int countWays(vector<vector<int>>& ranges) {
       long long n=ranges.size();
         long long mod=1e9+7;
-        long long i=0;
         int ans=2;  //beacuse 2 grps are gonna be there either empty or not 
         sort(ranges.begin(),ranges.end());
         int second=ranges[0][1];
@@ -11,12 +10,12 @@ public:
         {
             if(ranges[i][0]<=second)
             {
-                second=max(second,ranges[i][1]);
+                second=max(second,ranges[i][1]);  //merging
             }
             else
             {
-                second=ranges[i][1];
-                ans=(ans*2)%mod;
+                second=ranges[i][1];   // shifting to next interval
+                ans=(ans*2)%mod; 
             }
         }
         return ans;
