d39d38819ea959cf5e672fe45642f66af54aff76
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-18 09:47:08 +0530

    Time: 256 ms (17.91%), Space: 17.1 MB (26.72%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-         vector < vector < vector < int >>> dp(n, vector < vector < int >> (m, 
-  vector < int > (m, 0)));
-
-  for (int j1 = 0; j1 < m; j1++) {
-    for (int j2 = 0; j2 < m; j2++) {
-      if (j1 == j2)
-        dp[n - 1][j1][j2] = grid[n - 1][j1];
-      else
-        dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
-    }
-  }
-
-  //Outer Nested Loops for travering DP Array
-  for (int i = n - 2; i >= 0; i--) {
-    for (int j1 = 0; j1 < m; j1++) {
-      for (int j2 = 0; j2 < m; j2++) {
-
-        int maxi = INT_MIN;
-
-        //Inner nested loops to try out 9 options
-        for (int di = -1; di <= 1; di++) {
-          for (int dj = -1; dj <= 1; dj++) {
-
-            int ans;
-
-            if (j1 == j2)
-              ans = grid[i][j1];
-            else
-              ans = grid[i][j1] + grid[i][j2];
-
-            if ((j1 + di < 0 || j1 + di >= m) ||
-              (j2 + dj < 0 || j2 + dj >= m))
-
-              ans += -1e9;
-            else
-              ans += dp[i + 1][j1 + di][j2 + dj];
-
-            maxi = max(ans, maxi);
-          }
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,0)));
+        //base case
+        // tabulation done from m-1 ->0
+        for(int j1=0;j1<n;j1++)
+        {
+            for(int j2=0;j2<n;j2++)
+            {
+                if(j1==j2)
+                dp[m-1][j1][j2]=grid[m-1][j1];
+                else
+                dp[m-1][j1][j2]=grid[m-1][j1]+grid[m-1][j2];
+            }
         }
-        dp[i][j1][j2] = maxi;
-      }
-    }
-  }
-
-  return dp[0][0][m - 1];
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j1=0;j1<n;j1++)
+            {
+                for(int j2=0;j2<n;j2++)
+                {
+                    int maxi=INT_MIN;
+                    // for every cell 3*3 conditions
+                    for(int d1=-1;d1<=1;d1++)
+                    {
+                        for(int d2=-1;d2<=1;d2++)
+                        {
+                            int ans;
+                            if(j1==j2)
+                                ans=grid[i][j1];
+                            else
+                                ans=grid[i][j1]+grid[i][j2];
+                            //if out of bound
+                            if(j1+d1<0 or j1+d1>=n or j2+d2<0 or j2+d2>=n)
+                                ans+=INT_MIN;
+                            else
+                                ans+=dp[i+1][j1+d1][j2+d2];
+                             maxi=max(ans,maxi);
+                        }
+                    }
+                   dp[i][j1][j2]=maxi;
+                }
+            }
+        }
+        return dp[0][0][n-1];
     }
 };
