a205ced54db8b320357c8d63d42421dcf8b3294a
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-22 09:48:30 +0530

    Time: 47 ms (38.51%), Space: 18.5 MB (50.14%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int solve(int ind,int amount,vector<int> &coins,vector<vector<int>> &dp)
-    {
-        if(amount==0)
-            return 1;
-        if(ind==0)
-        {
-            if(amount%coins[ind]==0)
-                return 1;
-            return 0;
-        }
-        if(dp[ind][amount]!=-1)
-            return dp[ind][amount];
-        
-        int take=0,notake=0;
-        if(coins[ind]<=amount)
-            take=solve(ind,amount-coins[ind],coins,dp);
-        notake=solve(ind-1,amount,coins,dp);
-        return dp[ind][amount]=take+notake;
-    }
     int change(int amount, vector<int>& coins) {
-      int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(n-1,amount,coins,dp);  
+         int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        
+        for(int i=0;i<n;i++)
+            dp[i][0]=1;
+        
+        for(int i=0;i<=amount;i++)
+        {
+            if(i%coins[0]==0)
+                dp[0][i]=1;
+        }
+        
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<=amount;j++)
+            {
+               
+             int take=0,notake=0;
+             if(coins[i]<=j)
+            take=dp[i][j-coins[i]];
+        notake=dp[i-1][j];
+        dp[i][j]=take+notake;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
