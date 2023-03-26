aba1565b31da5e273b1b7977dd5c2082ef169451
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-26 22:33:46 +0530

    Time: 141 ms (41.18%), Space: 23.6 MB (52.94%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool isprime(int n)
+    {
+          if (n <= 1)
+        return false;
+        for(int i=2;i<n;i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    int find(int a,int b){
+        for(int i=2;i<a;i++)
+        {
+            if(isprime(i))
+            {
+                if((a-i)<b)
+                    return i;
+            }
+        }
+        return -1;
+    }
+    bool primeSubOperation(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+                continue;
+            else
+            {
+                int prime=find(nums[i],nums[i+1]);
+                if(prime==-1)
+                    return false;
+                nums[i]=nums[i]-prime;
+            }
+        }
+        return true;
+    }
+};
