f7fe69fe0bcca06651e6371926c92fb4357d9d5f
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-27 22:54:25 +0530

    Time: 110 ms (20.24%), Space: 11.4 MB (55.28%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
-    bool solve(int ind1,int ind2,string &s,string &p,vector<vector<int>> &dp)
-    {
-        if(ind1<0 and ind2<0)
-            return true;
-        if(ind1<0 and ind2>=0)
-        {
-            for(int i=0;i<=ind2;i++)
-                if(p[i]!='*')
-                    return false;
-            
-            return true;
-        }
-        if(ind2<0 and ind1>=0)
-            return false;
-        
-        if(dp[ind1][ind2]!=-1)
-            return dp[ind1][ind2];
-        
-        if(s[ind1]==p[ind2] or p[ind2]=='?')
-            return dp[ind1][ind2]=solve(ind1-1,ind2-1,s,p,dp);
-        
-        if(p[ind2]=='*')
-            return dp[ind1][ind2]=(solve(ind1,ind2-1,s,p,dp) or solve(ind1-1,ind2,s,p,dp));
-        
-        return dp[ind1][ind2]=false;
-        
-    }
     bool isMatch(string s, string p) {
-        int n=s.size();
+         int n=s.size();
         int m=p.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(n-1,m-1,s,p,dp);
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        
+        dp[0][0]=true;
+        
+        for(int i=1;i<=m;i++){
+        int flag = true;
+        for(int k=1;k<=i;k++){
+            if(p[k-1] != '*'){
+                flag = false;
+                break;
+            }
+        }
+        dp[0][i] = flag;
+    }
+        for(int i=1;i<=n;i++)
+            dp[i][0]=false;
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                    if(s[i-1]==p[j-1] or p[j-1]=='?')
+             dp[i][j]=dp[i-1][j-1];
+        
+               else if(p[j-1]=='*')
+            dp[i][j]=(dp[i][j-1] or dp[i-1][j]);
+                
+                else
+                    dp[i][j]=false;
+             }
+        }
+        
+        return dp[n][m];
     }
 };
