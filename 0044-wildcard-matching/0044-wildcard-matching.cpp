bd838879fddeaa05ce6b94e0c4a101648880d01c
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-27 22:57:45 +0530

    Time: 72 ms (51.33%), Space: 6.7 MB (94.29%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-         int n=s.size();
-        int m=p.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
-        
-        dp[0][0]=true;
-        
-        for(int i=1;i<=m;i++){
-        int flag = true;
-        for(int k=1;k<=i;k++){
-            if(p[k-1] != '*'){
-                flag = false;
-                break;
-            }
-        }
-        dp[0][i] = flag;
+bool isAllStars(string & S1, int i) {
+
+  // S1 is taken in 1-based indexing
+  for (int j = 1; j <= i; j++) {
+    if (S1[j - 1] != '*')
+      return false;
+  }
+  return true;
+}
+
+    bool isMatch(string S2, string S1) {
+
+   int n = S1.size();
+  int m = S2.size();
+
+  vector < bool > prev(m + 1, false);
+  vector < bool > cur(m + 1, false);
+
+  prev[0] = true;
+
+  for (int i = 1; i <= n; i++) {
+    cur[0] = isAllStars(S1, i);
+    for (int j = 1; j <= m; j++) {
+
+      if (S1[i - 1] == S2[j - 1] || S1[i - 1] == '?')
+        cur[j] = prev[j - 1];
+
+      else {
+        if (S1[i - 1] == '*')
+          cur[j] = prev[j] || cur[j - 1];
+
+        else cur[j] = false;
+      }
     }
-        for(int i=1;i<=n;i++)
-            dp[i][0]=false;
+    prev = cur;
+  }
+
+  return prev[m];
         
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                    if(s[i-1]==p[j-1] or p[j-1]=='?')
-             dp[i][j]=dp[i-1][j-1];
-        
-               else if(p[j-1]=='*')
-            dp[i][j]=(dp[i][j-1] or dp[i-1][j]);
-                
-                else
-                    dp[i][j]=false;
-             }
-        }
-        
-        return dp[n][m];
     }
 };
