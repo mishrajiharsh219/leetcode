ffb4b1cf72bb110c4058b65772dcc412421945bf
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-28 12:06:19 +0530

    Time: 4 ms (69.83%), Space: 9.5 MB (81.02%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    int solve(int ind,int validity,vector<int> &days,vector<int> &costs,vector<vector<int>> &dp)
+//     # a = cost[one day pass] + cost of next day
+// # b = cost[ week pass ] + cost of next day after week
+// # c = cost[ month pass ] + cost of next day after month
+    int solve(int ind,vector<int> &days,vector<int> &costs,vector<int> &dp)
     {
-        if(ind==0)
-        {
-            if(days[ind]>=validity)
-                return 0;
-            else
-                return *min_element(costs.begin(),costs.end());
-        }
-         if(dp[ind][validity]!=-1)
-             return dp[ind][validity];
-        if(days[ind]>=validity)
-            return dp[ind][validity]=solve(ind-1,validity,days,costs,dp);
-        
+        if(ind<0)
+            return 0;
+        if(dp[ind]!=-1)
+            return dp[ind];
         int case1=INT_MAX,case2=INT_MAX,case3=INT_MAX;
         
-        case1=costs[0]+solve(ind-1,days[ind],days,costs,dp);
+        case1=costs[0]+solve(ind-1,days,costs,dp);
         
-        case2=costs[1]+solve(ind-1,max(0,days[ind]-7+1),days,costs,dp);
         
-        case3=costs[2]+solve(ind-1,max(0,days[ind]-30+1),days,costs,dp);
+        int newindex=ind;
+        for(newindex=ind;newindex>=0 and days[newindex]>=(days[ind]-7+1);newindex--){}
+        case2=costs[1]+solve(newindex,days,costs,dp);
         
-        return dp[ind][validity]=min(case1,min(case2,case3));
+        for(newindex=ind;newindex>=0 and days[newindex]>=(days[ind]-30+1);newindex--){}
+        case3=costs[2]+solve(newindex,days,costs,dp);
+        
+        
+        return dp[ind]=min(case1,min(case2,case3));
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n=days.size();
-        int m=costs.size();
-        // int maxvalidity=days[0]-30+1;
-        vector<vector<int>> dp(n,vector<int>(367,-1));
-        return solve(n-1,days[n-1]+1,days,costs,dp);
+        vector<int> dp(366,-1);
+        return solve(n-1,days,costs,dp);
+        
     }
 };
