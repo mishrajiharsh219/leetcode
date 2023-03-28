fc5fbf030b0770d7c414676556085e4d509b0bde
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-28 12:20:06 +0530

    Time: 4 ms (69.83%), Space: 9.4 MB (91.47%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-//     # a = cost[one day pass] + cost of next day
-// # b = cost[ week pass ] + cost of next day after week
-// # c = cost[ month pass ] + cost of next day after month
-    int solve(int ind,vector<int> &days,vector<int> &costs,vector<int> &dp)
-    {
-        if(ind<0)
-            return 0;
-        if(dp[ind]!=-1)
-            return dp[ind];
-        int case1=INT_MAX,case2=INT_MAX,case3=INT_MAX;
-        
-        case1=costs[0]+solve(ind-1,days,costs,dp);
-        
-        
-        int newindex=ind;
-        for(newindex=ind;newindex>=0 and days[newindex]>=(days[ind]-7+1);newindex--){}
-        case2=costs[1]+solve(newindex,days,costs,dp);
-        
-        for(newindex=ind;newindex>=0 and days[newindex]>=(days[ind]-30+1);newindex--){}
-        case3=costs[2]+solve(newindex,days,costs,dp);
-        
-        
-        return dp[ind]=min(case1,min(case2,case3));
-    }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n=days.size();
-        vector<int> dp(366,-1);
-        return solve(n-1,days,costs,dp);
+        vector<int> dp(366,0);
         
+       int k=0;
+        
+        for(int i=1;i<366;i++)
+        {
+            dp[i]=dp[i-1];
+            if(i==days[k])
+            {
+                dp[i]=min(costs[0]+dp[i-1],min(costs[1]+dp[max(0,i-7)],costs[2]+dp[max(0,i-30)]));
+                if(k<n-1)
+                k++;
+            }
+            
+        }
+        return dp[365];
     }
 };
