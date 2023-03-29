ab99ae605708ec1d57650ee5c2213dcaf4eec5de
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-29 16:04:09 +0530

    Time: 12 ms (17.68%), Space: 11.8 MB (62.41%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -8,19 +8,9 @@ public:
         
         for(int ind=n-1;ind>=0;ind--)
         {
-            for(int buy=0;buy<=1;buy++)
-            {
-                int profit=0;
-        if(buy)
-        {
-            profit=max(-prices[ind]+dp[ind+1][0],0+dp[ind+1][1]);
-        }
-        else
-        {
-            profit=max(prices[ind]+dp[min(ind+2,n)][1],0+dp[ind+1][0]);
-        }
-         dp[ind][buy]=profit;
-            }
+        dp[ind][1]=max(-prices[ind]+dp[ind+1][0],0+dp[ind+1][1]);
+
+        dp[ind][0]=max(prices[ind]+dp[min(ind+2,n)][1],0+dp[ind+1][0]);
         }
         return dp[0][1];
     }
