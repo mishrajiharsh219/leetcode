9cca2356ef854da211c4a1696b227f31f727a3bf
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-30 12:35:52 +0530

    Time: 351 ms (49.99%), Space: 10.6 MB (39.46%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,18 +5,36 @@ public:
         if(n==0)
             return 0;
         vector<int> dp(n,1);
+        vector<int> hash(n);
         int maxi=1;
+        int lastindex=0;
         for(int i=0;i<n;i++)
         {
+            hash[i]=i;
             for(int prev=0;prev<i;prev++)
             {
-                if(nums[prev]<nums[i])
+                if(nums[prev]<nums[i] and 1+dp[prev]> dp[i])
                 {
-                    dp[i]=max(dp[i],1+dp[prev]);
+                    hash[i]=prev;
+                    dp[i]=1+dp[prev];
                 }
             }
-             maxi=max(maxi,dp[i]);
+             if(dp[i]>maxi)
+             {
+              maxi=dp[i];
+             lastindex=i;
+             }
         }
+        vector<int> lis;
+        lis.push_back(nums[lastindex]);
+        while(hash[lastindex]!=lastindex)
+        {
+            lastindex=hash[lastindex];
+            lis.push_back(nums[lastindex]);
+        }
+        reverse(lis.begin(),lis.end());
+        for(auto c:lis)
+            cout<<c<<" ";
         return maxi;
     }
 };
