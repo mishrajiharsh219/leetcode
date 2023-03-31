d2cc8df1cb1405ad226629ab4f73f65ca78ba838
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-03-31 10:02:28 +0530

    Time: 111 ms (70.66%), Space: 13.3 MB (86.28%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    static bool cmp(string &a,string &b)
+    {
+        return a.size()<b.size();
+    }
+    bool compare(string &a,string &b)
+    {
+        if(a.size()!=b.size()+1)
+            return false;
+        int first=0;
+        int second=0;
+        while(first<a.size())
+        {
+            if(a[first]==b[second])
+            {
+                first++;
+                second++;
+            }
+            else
+            {
+                first++;
+            }
+        }
+        if(first==a.size() and second==b.size())
+            return true;
+        return false;
+    }
+    int longestStrChain(vector<string>& words) {
+        int n=words.size();
+        if(words.size()==0)
+            return 0;
+        vector<int> dp(n,1);
+        sort(words.begin(),words.end(),cmp);
+        int maxi=1;
+        for(int i=0;i<n;i++)
+        {
+            for(int prev=0;prev<i;prev++)
+            {
+                if(compare(words[i],words[prev]))
+                {
+                    dp[i]=max(1+dp[prev],dp[i]);
+                }
+            }
+            maxi=max(maxi,dp[i]);
+        }
+        return maxi;
+    }
+};
