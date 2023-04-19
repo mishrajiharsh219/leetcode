5226b0ff075d0d79c9c243bf61932ec196e67eb0
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-04-19 17:16:18 +0530

    Time: 200 ms (56.25%), Space: 44.1 MB (33.24%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    int solve(int num,int oper,unordered_map<int,vector<int>> &mp,vector<vector<int>> &dp)
-    {
-        if(oper==0)
-            return 1;
-        if(dp[num][oper]!=-1)
-            return dp[num][oper]%mod;
-        int ways=0;
-        vector<int> temp=mp[num];
-        for(auto c:temp)
-        {
-            ways=(ways%mod)+solve(c,oper-1,mp,dp)%mod;
-        }
-        return dp[num][oper]=ways%mod;
+        int MOD = 1e9 + 7;
+int knightDialer(int n) {
+    vector<vector<int>> nextButtons = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {0, 3, 9}, {}, {0, 1, 7}, {2, 6}, {1, 3}, {2, 4}};
+    vector<long> endsWith(10, 1);
+    while(--n) { 
+        vector<long> temp(10, 0);
+        for(int curr = 0; curr < 10; curr++)
+            for(int next : nextButtons[curr])
+                temp[next] = (temp[next] + endsWith[curr]) % MOD;
+        endsWith = temp;
     }
-    int knightDialer(int n) {
-        int count=n;
-        unordered_map<int,vector<int>> mp;
-        mp[1]={6,8};
-        mp[2]={7,9};
-        mp[3]={4,8};
-        mp[4]={3,9,0};
-        mp[6]={0,1,7};
-        mp[7]={2,6};
-        mp[8]={1,3};
-        mp[9]={2,4};
-        mp[0]={4,6};
-        mp[5]={};
-        vector<vector<int>> dp(10,vector<int>(n,-1));
-        int ways=0;
-        for(auto it:mp)
-        {
-            ways=(ways%mod)+solve(it.first,count-1,mp,dp)%mod;
-        }
-        return ways%mod;
+    return accumulate(endsWith.begin(), endsWith.end(), 0l) % MOD;
     }
 };
