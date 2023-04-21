1403f30fd7ab0928e84e994d6b3b91fc6d168327
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-04-21 22:32:43 +0530

    Time: 561 ms (5.19%), Space: 294.8 MB (5.19%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-   int dp[102][102][102];
     int mod=1e9+7;
-    int solve(int k,int i, int j,int n, int minProfit, vector<int>& group, vector<int>& profit)
+    int solve(int ind,vector<pair<int,int>> &mp,int men,int profit,int minprofit,vector<vector<vector<int>>> &dp)
     {
-        if(k==profit.size())
+        if(ind<0)
         {
-            if(j>=minProfit and n>=i) return 1;
+            if(men>=0 and profit>=minprofit)
+                return 1;
             return 0;
         }
-        else if(n<i) return 0;
-     
-        if(dp[k][i][j]!=-1) return  dp[k][i][j];
-        int include=0,notInclude=0;
-        notInclude= solve(k+1,i,j,n,minProfit,group,profit);
-        include=solve(k+1,i+group[k],min(j+profit[k],minProfit),n,minProfit,group,profit);
-        return  dp[k][i][j] = (include  % mod + notInclude  % mod )%mod;
+        if(dp[ind][men][profit]!=-1)
+            return dp[ind][men][profit];
+        int take=0,notake=0;
+        notake=solve(ind-1,mp,men,profit,minprofit,dp)%mod;
+        if(men-mp[ind].first>=0)
+        take=solve(ind-1,mp,men-mp[ind].first,min(minprofit,profit+mp[ind].second),minprofit,dp)%mod;
+        return dp[ind][men][profit]=((take)%mod+(notake)%mod)%mod;
     }
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,0,n,minProfit,group,profit);
-        
+    int profitableSchemes(int men, int minprofit, vector<int>& group, vector<int>& profit) {
+        vector<pair<int,int>> mp;
+        int totalprofit=0;
+        for(int i=0;i<group.size();i++)
+        {
+        mp.push_back({group[i],profit[i]});
+            totalprofit+=profit[i];
+        }
+        int n=mp.size();
+        vector<vector<vector<int>>> dp(102,vector<vector<int>>(102,vector<int>(102,-1)));
+        return solve(n-1,mp,men,0,minprofit,dp)%mod;
     }
 };
