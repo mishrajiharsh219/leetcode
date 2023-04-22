d98339850204477b3feb46b6449c8b9d6baf3dcf
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-04-22 17:12:15 +0530

    Time: 310 ms (33.44%), Space: 231.9 MB (19.36%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    //check memo soln for my real thinking and mle soln
-    //let dp[i][j] denote at stone i, the frog can or cannot make jump of size j
-    // The maximum jump size the frog can make at each stone if possible is shown as followings: 
-// stone:      0, 1, 2, 3, 4, 5
-// jump size:  1, 2, 3, 4, 5, 6 (suppose frog made jump with size k + 1 at each stone)
-
     bool canCross(vector<int>& stones) {
-        int N = stones.size();
-        vector<vector<bool>> dp(N, vector<bool> (N+1, false));
-        dp[0][1] = true;  //at index 0 jump of 1 is possible
         
-        for(int i = 1; i < N; ++i){
-            for(int j = 0; j < i; ++j){
-                int diff = stones[i] - stones[j];
-                if(diff > N || !dp[j][diff]) 
-                    continue;
-                
-                if(i == N - 1) 
-                    return true;
-                
-                dp[i][diff] = true;
-                if(diff - 1 >= 0) dp[i][diff - 1] = true;
-                if(diff + 1 <= N) dp[i][diff + 1] = true;
+        int n = stones.size();
+        
+        vector<vector<int>>dp(n, vector<int>(n, false));
+        dp[0][1] = true;
+        
+        for (int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                int d = stones[i] - stones[j];
+                if (d > 0 && d < n && dp[j][d]) {
+                    if (i == n-1) return true;
+                    dp[i][d] = true;
+                    if (d+1 < n) dp[i][d+1] = true;
+                    if (d-1 < n) dp[i][d-1] = true;
+                }
             }
         }
-
+        
         return false;
     }
 };
