196826d0963942c1605dd1a917fee21e10edf696
Harsh Mishra <50453715+mishrajiharsh219@users.noreply.github.com>
2023-04-23 23:54:52 +0530

    Time: 266 ms (40.81%), Space: 20.9 MB (92.71%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool canCross(vector<int>& stones) {
+        // index->0 to n-2
+            // jump-> 1 to n-1
         int n=stones.size();
-        vector<vector<bool>> dp(n,vector<bool> (n,false));
-         dp[0][1]=true;
-        for(int i=1;i<n;i++)
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        dp[0][1]=1;
+        for(int i=1;i<stones.size();i++)
         {
-        for(int prevind=0;prevind<i;prevind++)
-        {
-            int dis=stones[i]-stones[prevind];
-            if(dis>0 and dis<n and dp[prevind][dis])
+            for(int j=0;j<i;j++)
             {
-             if(i==n-1)
-                 return true;
-            dp[i][dis]=true;
-            if(dis-1>=0) dp[i][dis-1]=true;
-            if(dis+1<n) dp[i][dis+1]=true;
+             int dis=stones[i]-stones[j];
+                if(dis<=0 or dis>=n or !dp[j][dis])
+                    continue;
+                if(i==n-1)
+                    return true;
+                dp[i][dis]=true;
+                if(dis-1>=0) dp[i][dis-1]=true;
+                if(dis+1<n)dp[i][dis+1]=true;
             }
-        }
         }
         return false;
     }
